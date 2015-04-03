e52a95455c731ddd4eadfa52bf5ab40756d4aea1
Matthew Jee <matthew@marmphco.com>
2015-04-02 22:51:33 -0700

    adds function to load configurations with rows per subject per period

--- js/configManager.js
+++ js/configManager.js
@@ -12,6 +12,54 @@ Redwood.factory("ConfigManager", function() {
             }
             
             return config;
+        },
+        loadPerSubject: function(rs, defaults) {
+            var config = {};
+
+            // load experiment defaults
+            for (var key in defaults) {
+                config[key] = defaults[key];
+            }
+
+            // get all configs for current period
+            var configs = rs.configs.filter(function(config) {
+                return config.period === rs.period;
+            });
+
+            // get default config for current period
+            var periodDefaultConfig = configs.filter(function(config) {
+                return config.subject === "default";
+            })[0];
+
+            if (periodDefaultConfig) {
+                // load period default config
+                for (var key in periodDefaultConfig) {
+                    config[key] = periodDefaultConfig[key];
+                }
+            }
+
+            // get subject specific config
+            var subjectConfig = configs.filter(function(config) {
+                if (!config.hasOwnProperty("subject")) {
+                    console.log("Warning: Configuration does not have a 'subject' field.")
+                }
+                if (config.subject instanceof Array) {
+                    return config.subject.filter(function(id) {
+                        return id.toString() === rs.user_id.toString();
+                    }).length > 0;
+                } else {
+                    return config.subject.toString() === rs.user_id.toString();
+                }
+            })[0];
+
+            if (subjectConfig) {
+                // load subject config
+                for (var key in subjectConfig) {
+                    config[key] = subjectConfig[key]
+                }
+            }
+            
+            return config;
         }
     }
 });
